955d7c3470b73458275d23039c5ea85511ed02a6
lmcuadrado <42412749+lmcuadrado@users.noreply.github.com>
2018-11-15 17:48:46 -0500

    Add files via upload

---
+++ ODE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int g=10;		//gravedad
+double c=0.2;  //Coeficiente de friccion
+double m=0.2; //masa
+int v=300;		//magnitud de la velocidad
+int alpha=45; 	//angulo en grados respecto a la horizontal
+int x0=0; 		//Condicion inicial
+
+int main()
+{
+
+return 0; 
+}
