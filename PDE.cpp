330033ee6219b8cfaa0704ce2a02fdac4a81ef6e
lmcuadrado <42412749+lmcuadrado@users.noreply.github.com>
2018-11-15 18:22:10 -0500

    Add files via upload

---
+++ PDE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+//Revisar bien unidades
+double k=1.62; //Conductividad termica
+double cp=820.0; //Calor especifico
+double rho=2.71; //rho
+int T=100; //temperatura
+double A=0.5; //altura metros
+double B=0.5;	//base metros
+
+
+int main()
+{
+
+return 0; 
+}
