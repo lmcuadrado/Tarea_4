8929578a4666b1e2822d4c26e467847aa42003a0
lmcuadrado <42412749+lmcuadrado@users.noreply.github.com>
2018-11-19 12:18:39 -0500

    Add files via upload

--- PDE.cpp
+++ PDE.cpp
@@ -26,7 +26,7 @@ void circulo(float area[][c],int radio, int x,int y);
 void casos(float area[][c],float T,int caso);
 void c1(float area[][c]);
 void c2(float area[][c]);
-void documento(float area[][c], string nombre);
+void creartxt(float area[][c], string nombre);
 double promedio(float area[][c]);
 
 int main()
@@ -35,7 +35,7 @@ int main()
 	return 0;
 }
 
-void documento(float area[][c], const char* nombre)
+void creartxt(float area[][c], const char* nombre)
 {
 
    FILE *output;      
@@ -68,12 +68,74 @@ double promedio(float area[][c])
     return sumita/(49 * 49);
 }
 
+void c1(float area[][c]) //Fijas
+{
+	int cont=0;
+    
+	if(cont<=N)
+	{
+	
+		for (int i=1 ; i<f-2 ; i++)
+		{
+			for (int j = 1 ; j<c-2 ; j++)
+			{
+				circulo(area,radio,x,y);
+				area[i][j]=(1-(4*dt*v)/(h*h))*area[i][j]+(dt*v/h*h)*(area[i+1][j]+area[i][j+1]+area[i-1][j]+area[i][j-1]);
+			casos(area,10,1);
+			}
+		}
+		
+		if (cont==1)
+		{
+			creartxt(area,"fija.txt");
+			
+		}
+	else if(cont==500)
+		{
+			creartxt(area,"fija.txt");
+
+		}
+	else if(cont==1000)
+		{
+			creartxt(seccion,"fija.txt");
+		}
+	else if(cont==2000)
+		{
+			creartxt(seccion,"fija.txt");
+		}
+	
+	}
+	cont+=1;
+	}
+}
+
+void c2(float area[][c])
+{
+	int cont= 0;
+    
+	casos(area,25,2);
+  	if(cont<=N)
+	{
+	
+	for (int i=1; i< f; i++)
+	{
+		for (int j= 1 ; j<c ; j++)
+			{
+				circulo(area,radio,x,y);
+				area[i][j]=(1-(4*dt*v)/(h*h))*area[i][j]+(dt*v/h*h)*(area[i+1][j]+area[i][j+1]+area[i-1][j]+area[i][j-1]);
+			}
+	}
+	
+	cont+=1;
+	}
+}
+
 
 
 void circulo(float area[][c],int radio, int x,int y)
 {
 
-	for(int i = x - radio; i <= x + radio; i++)
+	for(int i=x-radio; i <= x + radio; i++)
 	{
    
    		for(int j = y - radio; j <= y + radio; j++)
@@ -127,47 +189,6 @@ void casos(float area[][c],float T, int caso)
 		}
 }
 
-void c1(float area[][c])
-{
-	int cont=0;
-    
-	if(cont<=N)
-	{
-	
-	for (int i=1 ; i<f ; i++)
-	{
-			for (int j = 1 ; j < c ; j++)
-			{
-				circulo(area,radio,x,y);
-				area[i][j]=(1-(4*dt*v)/(h*h))*area[i][j]+(dt*v/h*h)*(area[i+1][j]+area[i][j+1]+area[i-1][j]+area[i][j-1]);
-			casos(area,10,1);
-			}
-	}
-
-	cont+=1;
-	}
-}
-
-void c2(float area[][c])
-{
-	int cont= 0;
-    
-	casos(area,25,2);
-  	if(cont<=N)
-	{
-	
-	for (int i=1; i< f; i++)
-	{
-		for (int j= 1 ; j<c ; j++)
-			{
-				circulo(area,radio,x,y);
-				area[i][j]=(1-(4*dt*v)/(h*h))*area[i][j]+(dt*v/h*h)*(area[i+1][j]+area[i][j+1]+area[i-1][j]+area[i][j-1]);
-			}
-	}
-	
-	cont+=1;
-	}
-}
 
 
 
